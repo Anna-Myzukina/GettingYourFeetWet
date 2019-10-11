df667bcdf35d3759e40725239285b282582e2dc4
Elukoye <lukoyedith@gmail.com>
2019-10-11 08:07:28 -0700

    modified controller.rb and route.rb to goodbyeworld

--- environment/hello_app/app/controllers/application_controller.rb
+++ environment/hello_app/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
+  def goobye
+    render html: "goodbye, world!"
   end
 end
